2492c33e52a8ccbbd0595e80ffdd1d14d23894e3
joshjnix <129699348+joshjnix@users.noreply.github.com>
2023-04-02 19:07:21 -0500

    Create main.py

---
+++ main.py
@@ -0,0 +1,12 @@
+class Solution:
+    def strStr(self, haystack: str, needle: str) -> int:
+        if not needle:
+            return 0
+        for i in range(len(haystack) - len(needle) + 1):
+            for j in range(len(needle)):
+                if haystack[i+j] != needle[j]:
+                    break
+            else:
+                return i
+        return -1
+
